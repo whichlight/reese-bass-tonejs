e092b6059e3b3b7796820e235eca24e2271d8729
Kawandeep Virdee <whichlight@gmail.com>
2016-05-30 00:32:43 -0400

    added bass

--- sketch.js
+++ sketch.js
@@ -1,13 +1,10 @@
 //touch interaction var
-var m = {x:0, y:0};
 var noises = [];
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  grow = windowHeight;
   background(0);
-  initSynth();
 
   //disable default touch events for mobile
   var el = document.getElementsByTagName("canvas")[0];
@@ -21,6 +18,8 @@ function setup() {
   stroke(255);
   strokeWeight(10);
   line(0,height/1.6,width, height/1.6);
+
+  initSynth();
 }
 
 function windowResized() {
@@ -32,35 +31,17 @@ function pdefault(e){
 }
 
 function draw() {
-  update();
   render();
 }
 
 function update(){
-  //normalize interaction
-  m.x = max(touchX, mouseX);
-  m.y = max(touchY, mouseY);
-  m.pressed = mouseIsPressed || touchIsDown;
 
-  if(m.pressed){
-
-   base_note = map(m.y, 0,height, 80,20);
-   console.log(base_note);
-    noises.forEach(function(n){
-      n.setBaseNote(base_note);
-
-    });
-
-  background(0);
-  stroke(255);
-  strokeWeight(10);
-  line(0,m.y,width, m.y);
-  }
 }
 
 function render(){
 
 }
+
 
 
 
@@ -90,65 +71,64 @@ function initSynth(){
   }
 }
 
+var touchStarted= function(){
+  console.log(touchY);
+  background(0);
+  stroke(255);
+  strokeWeight(10);
+  line(0,touchY,width, touchY);
 
-function Noise(args){
-  this.note= args.base_note + args.offset;
-  this.offset = args.offset;
-  this.whiteNoise;
-  this.filter;
-  this.volume = args.volume;
-  this.init = function(){
-    var bufferSize = 4096; //Math.pow(2,13); //between 8 & 14
-    //make this a global var so it isnt garbage collected
-    this.whiteNoise = context.createScriptProcessor(bufferSize, 0, 2);
-    this.whiteNoise.onaudioprocess = function(e) {
-      var outputBuffer = e.outputBuffer;
-      for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
-        var outputData = outputBuffer.getChannelData(channel);
-        for (var i = 0; i < bufferSize; i++) {
-          outputData[i] = Math.random()*2-1;
-        }
-      }
-    }
+  f = map(touchY, height,0, 30, 200);
+  synth.triggerAttack(f);
+  console.log(f);
 
-
-    this.filter= context.createBiquadFilter();
-    this.filter.type = "lowpass";
-    this.filter.frequency.value = mtof(this.note);
-    this.filter.Q.value= 1;
-
-    this.gain = context.createGain();
-    this.gain.connect(context.destination);
-    this.gain.gain.value = this.volume;
-
-    this.whiteNoise.connect(this.filter);
-    this.filter.connect(this.gain);
-
-    var convolver = context.createConvolver();
-    console.log('create');
-
-  }
-
- this.setBaseNote = function(bn){
-    this.note = bn + this.offset;
-    this.filter.frequency.value = mtof(this.note);
-  }
-
-  this.init();
-
-
+  return false;
 }
 
-function mtof(m) {
-  return Math.pow(2, (m - 69) / 12) * 440;
+var touchMoved= function(){
+
+  background(0);
+  stroke(255);
+  strokeWeight(10);
+  line(0,touchY,width, touchY);
+  f = map(touchY, height,0, 30, 200);
+
+
+  detune = map(touchX,0,width, 0,50);
+
+  synth.detune.value = detune;
+  synth.triggerAttack(f);
+  synth2.triggerAttack(f);
+  console.log(f);
+
+
+
+  return false;
+}
+
+
+var touchEnded = function(){
+console.log('ended');
+
+  synth.triggerRelease();
+  synth2.triggerRelease();
+  return false;
 }
 
 var startSynth = function(){
-  base_note = 50;
 
-  noises.push(new Noise({base_note:base_note/2, offset:0, volume: 0.9}));
-  noises.push(new Noise({base_note:base_note, offset:0, volume: 0.9}));
-  noises.push(new Noise({base_note:base_note,  offset:16, volume: 0.5}));
-  noises.push(new Noise({base_note:base_note,  offset:12, volume: 0.5}));
+  Tone.Master.volume.value=-10;
+
+  synth = new Tone.MonoSynth({"detune":25, "filterEnvelope":{
+  "attack":0, "decay":0, "sustain":0, "release":0, "baseFrequency":200}
+  }).toMaster();
+
+  synth2 = new Tone.MonoSynth({"detune":25, "filterEnvelope":{
+  "attack":0, "decay":0, "sustain":0, "release":0, "baseFrequency":200}
+  }).toMaster();
+
+  synth.detune.value = 25;
+
 }
+
 
