7794fe7cdcd3e4ad8303d4704ba0d22fb913430e
whichlight <whichlight@gmail.com>
2022-10-26 19:31:42 -0700

    updating with touch event

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 //touch interaction var
 var noises = [];
-
+var started = false;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,121 +14,121 @@ function setup() {
   el.addEventListener("touchleave", pdefault, false);
   el.addEventListener("touchmove", pdefault, false);
 
-
   stroke(255);
   strokeWeight(10);
-  line(0,height/1.6,width, height/1.6);
-
-  initSynth();
+  line(0, height / 1.6, width, height / 1.6);
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
-function pdefault(e){
-  e.preventDefault()
+function pdefault(e) {
+  e.preventDefault();
 }
 
 function draw() {
   render();
 }
 
-function update(){
+function update() {}
 
-}
+function render() {}
 
-function render(){
-
-}
-
-
-
-
-function initSynth(){
-  try{
-    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+function initSynth() {
+  try {
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
     context = new AudioContext();
 
-    var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false );
+    var iOS = navigator.userAgent.match(/(iPad|iPhone|iPod)/g) ? true : false;
     if (iOS) {
-      window.addEventListener('touchend', function() {
-        var buffer = context.createBuffer(1, 1, 22050);
-        var source = context.createBufferSource();
-        source.buffer = buffer;
-        source.connect(context.destination);
-        source.start(0);
-      }, false);
+      window.addEventListener(
+        "touchend",
+        function () {
+          var buffer = context.createBuffer(1, 1, 22050);
+          var source = context.createBufferSource();
+          source.buffer = buffer;
+          source.connect(context.destination);
+          source.start(0);
+        },
+        false
+      );
     }
-
-  }
-  catch (err){
-    alert('web audio not supported');
+  } catch (err) {
+    alert("web audio not supported");
   }
 
-  if(typeof(context)!="undefined"){
+  if (typeof context != "undefined") {
     startSynth();
   }
 }
 
-var touchStarted= function(){
+var touchStarted = function () {
+  if (!started) {
+    initSynth();
+  }
   console.log(touchY);
   background(0);
   stroke(255);
   strokeWeight(10);
-  line(0,touchY,width, touchY);
+  line(0, touchY, width, touchY);
 
-  f = map(touchY, height,0, 30, 200);
+  f = map(touchY, height, 0, 30, 200);
   synth.triggerAttack(f);
   console.log(f);
 
   return false;
-}
+};
 
-var touchMoved= function(){
-
+var touchMoved = function () {
   background(0);
   stroke(255);
   strokeWeight(10);
-  line(0,touchY,width, touchY);
-  f = map(touchY, height,0, 30, 200);
+  line(0, touchY, width, touchY);
+  f = map(touchY, height, 0, 30, 200);
 
-
-  detune = map(touchX,0,width, 0,50);
+  detune = map(touchX, 0, width, 0, 50);
 
   synth.detune.value = detune;
   synth.triggerAttack(f);
   synth2.triggerAttack(f);
   console.log(f);
 
-
-
   return false;
-}
+};
 
-
-var touchEnded = function(){
-console.log('ended');
+var touchEnded = function () {
+  console.log("ended");
 
   synth.triggerRelease();
   synth2.triggerRelease();
   return false;
-}
+};
 
-var startSynth = function(){
+var startSynth = function () {
+  Tone.Master.volume.value = -10;
 
-  Tone.Master.volume.value=-10;
-
-  synth = new Tone.MonoSynth({"detune":25, "filterEnvelope":{
-  "attack":0, "decay":0, "sustain":0, "release":0, "baseFrequency":200}
+  synth = new Tone.MonoSynth({
+    detune: 25,
+    filterEnvelope: {
+      attack: 0,
+      decay: 0,
+      sustain: 0,
+      release: 0,
+      baseFrequency: 200,
+    },
   }).toMaster();
 
-  synth2 = new Tone.MonoSynth({"detune":25, "filterEnvelope":{
-  "attack":0, "decay":0, "sustain":0, "release":0, "baseFrequency":200}
+  synth2 = new Tone.MonoSynth({
+    detune: 25,
+    filterEnvelope: {
+      attack: 0,
+      decay: 0,
+      sustain: 0,
+      release: 0,
+      baseFrequency: 200,
+    },
   }).toMaster();
 
   synth.detune.value = 25;
-
-}
-
-
+};
